c3016917b9d1c2e14c347beaf60e243def53a8e5
Harsha vardhan manoj Bikkisd <39381063+hbikki@users.noreply.github.com>
2018-08-31 13:00:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const api = require('./api/');
 const app = express();
-const path = require('path');
 const bodyParser = require('body-parser');
 const port = 3000;
-
-const virtualDirPath = process.env.virtualDirPath || '';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-
-app.use(virtualDirPath, express.static(path.join(__dirname, 'dist')));
-app.use(virtualDirPath + '/api', api);
+app.use(+ '/api', api);
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
